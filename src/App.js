102679c524d9bf77bdb6a08a2e040adef902f369
Harrison <harrisonshelnutt@gmail.com>
2022-04-25 14:29:01 -0600

    Added header with social links

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import './App.css';
+import Header from './components/Header';
+import theme from './theme';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header />
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,58 @@
+import { Box, Button, Container, Typography } from "@mui/material";
+import { GitHub, LinkedIn, Email, InsertDriveFile } from "@mui/icons-material";
+
+
+function Header() {
+    return (
+        <div>
+            <Container>
+                <Box sx={{ marginTop: '40px' }}>
+                    <Typography variant="h5" align="center">
+                        Harrison Shelnutt
+                    </Typography>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'center'
+                        }}
+                    >
+                        <Button
+                            key="LinkedIn"
+                            href="https://www.linkedin.com/in/jhshelnu/"
+                            target="_blank"
+                            endIcon={ <LinkedIn /> }
+                        >
+                            LinkedIn
+                        </Button>
+                        <Button
+                            key="GitHub"
+                            href="https://www.github.com/jhshelnu/"
+                            target="_blank"
+                            endIcon={ <GitHub /> }
+                        >
+                            GitHub
+                        </Button>
+                        <Button
+                            key="Email"
+                            href="mailto:h@rry.sh"
+                            target="_blank"
+                            endIcon={ <Email /> }
+                        >
+                            Email
+                        </Button>
+                        <Button
+                            key="Resume"
+                            href="/resume.pdf"
+                            target="_blank"
+                            endIcon={ <InsertDriveFile /> }
+                        >
+                            Resume
+                        </Button>
+                    </Box>
+                </Box>
+            </Container>
+        </div>
+    );
+}
+
+export default Header;
